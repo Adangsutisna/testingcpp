762df9a125982792313634392430ed596c49ad24
Adang Sutisna <adangsutisna@Adangs-MBP.lan>
2024-03-22 06:57:47 +0700

    update

--- src/portfolio.cpp
+++ src/portfolio.cpp
@@ -26,8 +26,8 @@ Money StockPortfolio::getOrigValue() const {
 
 Money StockPortfolio::getProfit() const {
     Money profit = getTotalValue() - getOrigValue();
-    total=adjustment(profit);
-    return total;
+    profit -= 0.01;
+    return profit;
 }
 
 std::vector<std::string> StockPortfolio::getAlphaList() {
